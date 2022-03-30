d967f5169accdcdf26688966ad034b970138f4d5
Felipe Santos <fezto.17@gmail.com>
2022-03-30 13:28:01 -0300

    Aplicado alguns estilos no Header

---
+++ AluraViagens/View/Header/HomeTableViewHeader.swift
@@ -0,0 +1,28 @@
+//
+//  HomeTableViewHeader.swift
+//  AluraViagens
+//
+//  Created by Felipe Santos on 24/03/22.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var banerImageView: UIImageView!
+    @IBOutlet weak var bannerView: UIView!
+    
+    func configuraView() {
+        headerView.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
+        
+        bannerView.layer.cornerRadius = 10
+        bannerView.layer.masksToBounds = true
+        
+        headerView.layer.cornerRadius = 100
+        headerView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+    }
+}
