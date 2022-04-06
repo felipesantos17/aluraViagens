b473182e70285b8a6c904c110a071103122dee9d
Felipe Santos <fezto.17@gmail.com>
2022-04-05 21:24:54 -0300

    Implementado o modelo dos dados e criado os IBOutlets dos componentes das celulas

--- AluraViagens/View/ViewController.swift
+++ AluraViagens/View/ViewController.swift
@@ -13,29 +13,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configuraTableView()
+        view.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
+    }
+    
+    func configuraTableView() {
+        viagensTableView.register(UINib(nibName: "ViagemTableViewCell", bundle: nil), forCellReuseIdentifier: "ViagemTableViewCell")
         viagensTableView.dataSource = self
         viagensTableView.delegate = self
-        
-        view.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
     }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return sessaoDeViagens?[section].numeroDeLinhas ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
-        cell.textLabel?.text = "Viagem \(indexPath.row + 1)"
-        
-        return cell
+        guard let celulaViagem = tableView.dequeueReusableCell(withIdentifier: "ViagemTableViewCell") as? ViagemTableViewCell else { fatalError("erro to create ViagemTableViewCell")
+        }
+
+        return celulaViagem
     }
 }
 
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        
         let headerView = Bundle.main.loadNibNamed("HomeTableViewHeader", owner: self, options: nil)?.first as? HomeTableViewHeader
         headerView?.configuraView()
         
@@ -44,5 +47,9 @@ extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 300
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
     }
 }
---
+++ AluraViagens/ViewModel/ViagemViewModel.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+enum ViagemViewModelType: String {
+    case destaques
+    case ofertas
+    case internacionais
+}
+
+protocol ViagemViewModel {
+    var tituloSessao: String { get }
+    var tipo: ViagemViewModelType { get }
+    var viagens: [Viagem] { get set }
+    var numeroDeLinhas: Int { get }
+}
