2f80d494c82ab93e17389b300862077007a30698
Felipe Santos <fezto.17@gmail.com>
2022-04-07 18:13:39 -0300

    Finalizado a tela inicial do APP

--- AluraViagens/View/ViewController.swift
+++ AluraViagens/View/ViewController.swift
@@ -32,8 +32,16 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let celulaViagem = tableView.dequeueReusableCell(withIdentifier: "ViagemTableViewCell") as? ViagemTableViewCell else { fatalError("erro to create ViagemTableViewCell")
         }
-
-        return celulaViagem
+        
+        let viewModel = sessaoDeViagens?[indexPath.section]
+        
+        switch viewModel?.tipo {
+        case .destaques:
+            celulaViagem.configuraCelula(viewModel?.viagens[indexPath.row])
+            return celulaViagem
+        default:
+            return UITableViewCell()
+        }
     }
 }
 
@@ -50,6 +58,6 @@ extension ViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 400
+        return UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? 400 : 475
     }
 }
